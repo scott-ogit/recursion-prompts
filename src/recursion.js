e475801cbd3a7a81eaade15b6a19ab477f9c20b4
Scott Maddock <scottowenmaddock@gmail.com>
2020-11-13 00:13:58 -0500

    finished 3... finished through 4...

--- src/recursion.js
+++ src/recursion.js
@@ -39,6 +39,22 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+
+    if (array.length === 0)
+    return 0;
+
+    var sum = 0;
+    var arr = array.slice();
+
+    for( var i = 0; i < arr.length; i++ ) {
+        if (Array.isArray(arr[i])) {
+            sum += arraySum(arr[i]);
+        }
+        else {
+            sum += arr[i];
+        }
+    }
+    return sum;
 };
 
 // 4. Check if a number is even.
