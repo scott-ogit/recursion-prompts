7dffc38db9b6435b9f8bca69b173b41f08a5c08e
Scott Maddock <scottowenmaddock@gmail.com>
2020-11-12 23:22:45 -0500

    prob 4 done... 3 not finished...

--- src/recursion.js
+++ src/recursion.js
@@ -43,6 +43,16 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+    
+    n = Math.abs(n);
+
+    if (n === 0) {
+        return true;
+    } else if ( n === 1) {
+        return false;
+    } else {
+        return isEven( n - 2 );
+    }
 };
 
 // 5. Sum all integers below a given integer.
