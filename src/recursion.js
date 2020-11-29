a99f561ace53bae318b722c1fcf0a204fb9b3f7b
Scott Maddock <scottowenmaddock@gmail.com>
2020-11-29 13:46:33 -0500

    problem 6 returns an array when all is well... work through prompts

--- src/recursion.js
+++ src/recursion.js
@@ -94,6 +94,19 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  
+  if (y - x === 2) {
+    
+    return [ x + 1 ];
+
+  } else {
+
+    var list = range(x, y - 1);
+    list.push(y - 1);
+    return list;
+
+  }
+  
 };
 
 // 7. Compute the exponent of a number.
