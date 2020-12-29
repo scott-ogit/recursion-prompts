cc268f872b62ea0ff287b22f8ca20906eeb2da15
Scott Maddock <scottowenmaddock@gmail.com>
2020-12-28 21:59:50 -0500

    significant progress on palindromes... skipped 8 + 9...

--- src/recursion.js
+++ src/recursion.js
@@ -179,6 +179,18 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+
+  if ( string.length <= 1 ) {
+    return true;
+  }
+
+  if ( string.slice(0, 1) !==  string.slice(-1) ) {
+    return false;
+  }
+
+  // recursive case
+  return palindrome(string.slice(1, -1))
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
